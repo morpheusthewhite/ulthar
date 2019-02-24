999cf662044040866b3720ac80b4bf0697a67e46
morpheusthewhite <zffromGerace@hotmail.it>
2019-02-24 17:18:17 +0100

    Update gui

--- include/gui.h
+++ include/gui.h
@@ -8,6 +8,13 @@
 #include <QPixmap>
 #include <QFormLayout>
 #include <QLineEdit>
+#include <QSpinBox>
+
+#define COORDINATES_PRECISION 3
+#define COORDINATES_MIN       -10
+#define COORDINATES_MAX       10
+#define COORDINATES_STEP      0.1
+
 class Gui : public QWidget
 {
     Q_OBJECT
@@ -20,12 +27,33 @@ public slots:
 
 private:
     QFrame* mLeftFrame;
-    QPushButton* mExportButton;
-    QLabel* mPreviewLabel;
-    QHBoxLayout* mLayout;
     QFormLayout* mLeftFrameLayout;
+
     QLabel* mDefinitionLabel;
-    QLineEdit* mDefinionLineEdit;
+    QSpinBox* mDefinionSpinBox;
+
+    QLabel* mStartLabel;
+    QDoubleSpinBox* mStartX;
+    QDoubleSpinBox* mStartY;
+    QHBoxLayout* mStartCoordinatesLayout;
+    QLabel* mEndLabel;
+    QDoubleSpinBox* mEndX;
+    QDoubleSpinBox* mEndY;
+    QHBoxLayout* mEndCoordinatesLayout;
+
+    QLabel* mOutPathLabel;
+    QLineEdit* mOutPathLineEdit;
+    QPushButton* mOutPathSelect;
+    QHBoxLayout* mOutPathLayout;
+
+    QPushButton* mExportButton;
+    QPushButton* mColorsButton;
+    QHBoxLayout* mButtonsLayout;
+    QFrame* mButtonsFrame;
+
+    QLabel* mPreviewLabel;
+
+    QHBoxLayout* mLayout;
 };
 
 #endif // GUI_H
--- src/gui.cpp
+++ src/gui.cpp
@@ -1,31 +1,80 @@
 #include "gui.h"
 
+QDoubleSpinBox* getCoordinatesSpinBox(QWidget* parent=nullptr){
+    QDoubleSpinBox* spinbox = new QDoubleSpinBox(parent);
+    spinbox->setDecimals(COORDINATES_PRECISION);
+    spinbox->setSingleStep(COORDINATES_STEP);
+    spinbox->setRange(COORDINATES_MIN, COORDINATES_MAX);
+
+    return spinbox;
+}
+
+
 Gui::Gui(QWidget *parent) : QWidget(parent)
 {
     /* initializing left box */
+    // left frame containing all widgets
     mLeftFrame = new QFrame(this);
-    mExportButton = new QPushButton("Export", mLeftFrame);
-    mDefinitionLabel = new QLabel("Definition", mLeftFrame);
-    mDefinionLineEdit = new QLineEdit(mLeftFrame);
-
-    /* setting layout of the left box */
+    // its layout
     mLeftFrameLayout = new QFormLayout();
-    mLeftFrameLayout->addWidget(mExportButton);
-    mLeftFrameLayout->addRow(mDefinitionLabel, mDefinionLineEdit);
 
+    // setting the row of the starting coordinates
+    mStartLabel = new QLabel("Start coordinates", mLeftFrame);
+    // setting up spinbox (the others will be bare copies of it)
+    mStartX = getCoordinatesSpinBox();
+    mStartY = getCoordinatesSpinBox();
+    mStartCoordinatesLayout = new QHBoxLayout();
+    mStartCoordinatesLayout->addWidget(mStartX);
+    mStartCoordinatesLayout->addWidget(mStartY);
+    mLeftFrameLayout->addRow(mStartLabel, mStartCoordinatesLayout);
+
+    // setting the row of the ending coordinates
+    mEndLabel = new QLabel("End coordinates", mLeftFrame);
+    mEndX = getCoordinatesSpinBox();
+    mEndY = getCoordinatesSpinBox();
+    mEndCoordinatesLayout = new QHBoxLayout();
+    mEndCoordinatesLayout->addWidget(mEndX);
+    mEndCoordinatesLayout->addWidget(mEndY);
+    mLeftFrameLayout->addRow(mEndLabel, mEndCoordinatesLayout);
+
+    // row for asking for the definition of the final image
+    mDefinitionLabel = new QLabel("Definition", mLeftFrame);
+    mDefinionSpinBox = new QSpinBox(mLeftFrame);
+    mDefinionSpinBox->setMaximum(100);
+    mLeftFrameLayout->addRow(mDefinitionLabel, mDefinionSpinBox);
+
+    mOutPathLabel = new QLabel("Saving to");
+    mOutPathLineEdit = new QLineEdit();
+    mOutPathSelect = new QPushButton("Select Folder");
+    mOutPathLayout = new QHBoxLayout();
+    mOutPathLayout->addWidget(mOutPathLineEdit);
+    mOutPathLayout->addWidget(mOutPathSelect);
+    mLeftFrameLayout->addRow(mOutPathLabel, mOutPathLayout);
+
+    // the export button
+    mColorsButton = new QPushButton("Colors");
+    mExportButton = new QPushButton("Export");
+    mButtonsLayout = new QHBoxLayout();
+    mButtonsLayout->addWidget(mColorsButton);
+    mButtonsLayout->addWidget(mExportButton);
+    mButtonsFrame = new QFrame();
+    mButtonsFrame->setLayout(mButtonsLayout);
+    mLeftFrameLayout->addWidget(mButtonsFrame);
+
+    // setting the layout for the left box
     mLeftFrame->setLayout(mLeftFrameLayout);
 
-
     /* setting right box */
-    mPreviewLabel = new QLabel(this);
+    mPreviewLabel = new QLabel("Label di prova", this);
     QPixmap pixmap("out.jpeg");
     mPreviewLabel->setPixmap(pixmap);
 
     /* setting layout of the right box */
     mLayout = new QHBoxLayout();
-    mLayout->addWidget(mExportButton);
+    mLayout->addWidget(mLeftFrame);
     mLayout->addWidget(mPreviewLabel);
 
     this->setLayout(mLayout);
+
 }
 
